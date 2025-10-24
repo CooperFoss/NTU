065057c4981789a86255eca47eba455d94fea9b5
cooper <cfoss001@stpaul.k12.mn.us>
2025-10-24 07:12:15 -0500

    home ball game m

--- src/Player.java
+++ src/Player.java
@@ -10,10 +10,36 @@ public class Player {
         this.z = z;
     }
     public void move(float xDisp, float yDisp){
-        x+=xDisp;
-        y+=yDisp;
+        this.x+=xDisp;
+        if (this.x<0 || this.x>100) this.x=50;
+        this.y+=yDisp;
+        if (this.y<0 || this.y>100) this.y=50;
     }
     public void jump(float zDisp){
-        z = zDisp;
+        z += zDisp;
+    }
+    public boolean near(Ball ball){
+        float distance = (float)  Math.sqrt(Math.pow(ball.getX()-this.x,2)+Math.pow(ball.getY()-this.y,2));
+        return (distance<8);
+    }
+    public void kick(Ball ball){
+        float kickx;
+        if (number==3 || number==4)
+                kickx = (float) (Math.random()*-20);
+        else kickx = (float) (Math.random()*20);
+        float kicky;
+                kicky = (float) (Math.random()*20-10);
+                ball.setX(ball.getX()+kickx);
+                ball.setY(ball.getY()+kicky);
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "number=" + number +
+                ", x=" + x +
+                ", y=" + y +
+                ", z=" + z +
+                '}';
     }
 }
